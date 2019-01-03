64e1b92dd7ced35b5851ee9b586bb4c853366665
DiogoC98 <diogocruz12345@hotmail.com>
2019-01-03 16:28:26 +0000

    carinho

--- app/src/main/java/pt/ipleiria/estg/dei/amsi/sound3application/Fragments/FavoritosFragment.java
+++ app/src/main/java/pt/ipleiria/estg/dei/amsi/sound3application/Fragments/FavoritosFragment.java
@@ -3,11 +3,8 @@ package pt.ipleiria.estg.dei.amsi.sound3application.Fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,18 +14,16 @@ import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
-import adaptadores.AlbumAdapter;
 import adaptadores.AlbumPesquisaAdapter;
 import adaptadores.ArtistaPesquisaAdapter;
 import adaptadores.GeneroAdapter;
 import adaptadores.MusicaAdapter;
-import adaptadores.ViewPagerAdapter;
 
-import adaptadores.ViewPagerAdapter;
 import models.Album;
 import models.Artista;
 import models.Genero;
 import models.Musica;
+
 import pt.ipleiria.estg.dei.amsi.sound3application.R;
 
 public class FavoritosFragment extends Fragment {
