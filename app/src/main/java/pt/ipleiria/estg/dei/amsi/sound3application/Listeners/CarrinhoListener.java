b9360fcd93c99df65cd1af7bc3aee6b1ee4ace4e
DiogoC98 <diogocruz12345@hotmail.com>
2019-01-27 03:20:19 +0000

    popup

--- app/src/main/java/pt/ipleiria/estg/dei/amsi/sound3application/Listeners/CarrinhoListener.java
+++ app/src/main/java/pt/ipleiria/estg/dei/amsi/sound3application/Listeners/CarrinhoListener.java
@@ -2,8 +2,11 @@ package pt.ipleiria.estg.dei.amsi.sound3application.Listeners;
 
 import java.util.ArrayList;
 
+import models.Album;
+import models.FavoritoMusica;
 import models.Musica;
 
 public interface CarrinhoListener {
-    void onRefreshCarrinho(ArrayList<Musica> musicas);
+    void onRefreshCarrinho(ArrayList<Musica> musicas, ArrayList<Album> album);
+    void onRefreshMusicasFavoritos(ArrayList<Musica> favoritoMusicas);
 }
